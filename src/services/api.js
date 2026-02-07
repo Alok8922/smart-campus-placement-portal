bc4410ca1dc526623883aeec088437ae92ee934f
Alok Kumar <alokkumar.cbi.cs@gmail.com>
2026-02-07 11:57:33 +0530

    Deploy backend fixes and MongoDB config

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://placement-backend-ta2e.onrender.com/api",
 });
 
 // 🔐 Attach token ONLY if it exists
